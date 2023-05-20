4d5debc33fe8b1b5f3967a4bf90d021ebb658076
Wesley Moraes <wesleycrz@hotmail.com>
2023-05-20 02:04:11 -0300

    logica construida

--- js/script.js
+++ js/script.js
@@ -7,6 +7,39 @@ var chars = {
     Driud : "Aephestos"
 }
 
+/*Objetos*/
+
+var drawAtr = {
+    for : "14",
+    dex : "8",
+    con : "12",
+    int : "14",
+    wis : "16",
+    cha : "12"
+}
+
+var mazabeleAtr = {
+    for : "10",
+    dex : "18",
+    con : "14",
+    int : "12",
+    wis : "14",
+    cha : "10"
+}
+
+
+
+function identifyObj(char){
+    var atributesValues = Object.values(char);
+    document.getElementById("atr-for").innerHTML = atributesValues[0];
+    document.getElementById("atr-dex").innerHTML = atributesValues[1];
+    document.getElementById("atr-con").innerHTML = atributesValues[2];
+    document.getElementById("atr-int").innerHTML = atributesValues[3];
+    document.getElementById("atr-wis").innerHTML = atributesValues[4];
+    document.getElementById("atr-cha").innerHTML = atributesValues[5];
+
+    //return atributesValues;
+}
 
 document.addEventListener("DOMContentLoaded", function(){
     // for (const chave in chars){
@@ -31,5 +64,53 @@ document.addEventListener("DOMContentLoaded", function(){
     //     document.getElementById("CharsList").appendChild(item);
 
     // }
+
+
+
+    document.getElementsByClassName("content-info-atributes")[0].style.display = "block";
+    document.getElementsByClassName("content-info-relationship")[0].style.display = "none";
+    document.getElementsByClassName("content-info-favorite-magics")[0].style.display = "none";
+
+    //eventos de click
+    document.getElementById("li-atributes").addEventListener("click", function(){
+        document.getElementsByClassName("content-info-atributes")[0].style.display = "block";
+        document.getElementsByClassName("content-info-relationship")[0].style.display = "none";
+        document.getElementsByClassName("content-info-favorite-magics")[0].style.display = "none";
+    });
+
+    document.getElementById("li-relationship").addEventListener("click", function(){
+        document.getElementsByClassName("content-info-atributes")[0].style.display = "none";
+        document.getElementsByClassName("content-info-relationship")[0].style.display = "block";
+        document.getElementsByClassName("content-info-favorite-magics")[0].style.display = "none";
+    });
+    
+    document.getElementById("li-favs-magics").addEventListener("click", function(){
+        document.getElementsByClassName("content-info-atributes")[0].style.display = "none";
+        document.getElementsByClassName("content-info-relationship")[0].style.display = "none";
+        document.getElementsByClassName("content-info-favorite-magics")[0].style.display = "block";
+    });
+
+    //formacao da pagina
+    var charsPage = document.getElementsByTagName("body");
+    var objetoStringChar = charsPage[0].id;
+    console.log(typeof objetoStringChar);
+
+    if(objetoStringChar == "drawPageId"){
+        atributesValues = identifyObj(drawAtr);
+        //var atributesValues = identifyObj(drawAtr);
+
+    }else if(objetoStringChar == "mazabelePageId"){
+        atributesValues = identifyObj(mazabeleAtr);
+
+    }else if(objetoStringChar == "aephestoslePageId"){
+        atributesValues = identifyObj(aephestosAtr);
+
+    }else if(objetoStringChar == "gregorioPageId"){
+        atributesValues = identifyObj(gregorioleAtr);
+
+    }else if(objetoStringChar == "zaosPageId"){
+        atributesValues = identifyObj(zaosAtr);
+
+    }
 
 });
